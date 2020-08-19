3a717ac342e02f9a7b2ba169762fca6cbf3ffc6c
Portmanat <Amrahaziz.1878@gmail.com>
2020-08-19 11:20:20 +0400

    Initial Commit

---
+++ CustomAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAlertController
+//
+//  Created by Ilhad on 8/19/20.
+//  Copyright © 2020 KeenCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
